956cc69ae004832545696bb6a78056d0b84e8ae7
stefanmzeidler <stefanmzeidler@gmail.com>
2026-01-11 14:28:29 -0600

    Added ability to save/load dataset locally.
    Otherwise would need to load from HuggingFace each time.

--- docretriever.py
+++ docretriever.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
+import json
 
 class DocRetriever(ABC):
     @abstractmethod
@@ -16,3 +18,10 @@ class DocRetriever(ABC):
     @abstractmethod
     def load_embeddings(self,**kwargs):
         pass
+
+    @staticmethod
+    def get_data_path(datatype):
+        project_root = Path(__file__).resolve().parent
+        with open(project_root / "config.json", "r", encoding="utf-8") as f:
+            cfg = json.load(f)
+        return project_root / cfg["paths"]["data"][datatype]

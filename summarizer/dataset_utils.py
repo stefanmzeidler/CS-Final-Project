652eef0ef42362ff7750935cbbb6d9e8a39e2a68
stefanmzeidler <stefanmzeidler@gmail.com>
2026-01-12 15:21:26 -0600

    Updated get_data_path and load_local.
    Now reflect variable path in config.json. Also allow for user to specify whether local resource is a dataset or embeddings.

--- summarizer/dataset_utils.py
+++ summarizer/dataset_utils.py
@@ -1,26 +1,31 @@
 import os
-from datasets import load_dataset
+from typing import Any
+
+from datasets import load_dataset, load_from_disk, Dataset, DatasetDict
 from pathlib import Path
 import json
+import torch
 
-def xml_to_arrow(source: str | os.PathLike[str], target: str | os.PathLike[str], max_shard_size:str = "100MB"):
+
+def xml_to_arrow(source: str | Path, target: str | Path, max_shard_size: str = "100MB"):
     """
     Converts an xml file to an arrow file.
     :param source: The source directory containing the xml files.
     :param target: The target parquet file.
     :param max_shard_size: The maximum size of each shard. Must have a number followed by unit, e.g., "100 MB"
     """
-    dataset = load_dataset("xml", data_dir = source)
-    dataset.save_to_disk(target,max_shard_size = max_shard_size)
+    dataset = load_dataset("xml", data_dir=source)
+    dataset.save_to_disk(target, max_shard_size=max_shard_size)
 
-def get_data_path(datatype):
+
+def get_data_path(dataset_name: str, data_type: str) -> str | Path:
+    if data_type != "dataset" and data_type != "embeddings":
+        raise ValueError("datatype must be 'dataset' or 'embeddings'")
     project_root = Path(__file__).resolve().parent.parent
     with open(project_root / "config.json", "r", encoding="utf-8") as f:
         cfg = json.load(f)
-    return project_root / cfg["paths"]["data"][datatype]
+    return project_root / cfg["paths"]["data"].format(dataset_name=dataset_name, data_type=data_type)
 
-def load_local(dataset):
-    project_root = Path(__file__).resolve().parent.parent
-    with open(project_root / "config.json", "r", encoding="utf-8") as f:
-        cfg = json.load(f)
-    return project_root / cfg["paths"]["data"]["datasets"][dataset]
+def load_local(dataset_name: str, data_type: str) -> Dataset | DatasetDict | Any:
+    path = get_data_path(dataset_name, data_type)
+    return load_from_disk(path) if data_type == "dataset" else torch.load(path / "embeddings.pt")

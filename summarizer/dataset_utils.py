5f27a5e15ebe0356f6e38881387501a2f7696dfa
stefanmzeidler <stefanmzeidler@gmail.com>
2026-01-14 22:56:33 -0600

    Implemented function to convert PMC xml files to arrow files.
    The PMC subset dataset consists of individual XML files. A way was needed to convert these into a dataset and save it in a format for machine learning/AI.

--- summarizer/dataset_utils.py
+++ summarizer/dataset_utils.py
@@ -1,22 +1,40 @@
 import os
 from typing import Any
-
-from datasets import load_dataset, load_from_disk, Dataset, DatasetDict
+from datasets import load_from_disk, Dataset, DatasetDict
 from pathlib import Path
 import json
 import torch
+from lxml import etree
 
 
-def xml_to_arrow(source: str | Path, target: str | Path, max_shard_size: str = "100MB"):
+def xml_to_arrow(source: str | Path, target: str | Path, max_shard_size = "100MB"):
     """
-    Converts an xml file to an arrow file.
+    Converts PMC xml files to a arrow files.
     :param source: The source directory containing the xml files.
-    :param target: The target parquet file.
-    :param max_shard_size: The maximum size of each shard. Must have a number followed by unit, e.g., "100 MB"
+    :param target: The target parquet directory for the arrow files.
+    :param max_shard_size: The maximum size of the shards to create. Must be in "100MB" format.
     """
-    dataset = load_dataset("xml", data_dir=source)
-    dataset.save_to_disk(target, max_shard_size=max_shard_size)
+    def iter_pmc_articles(xml_dir):
+        def check_text(text_list):
+            return None if len(text_list) == 0 else " ".join(text_list).strip()
 
+        for xml_file in Path(xml_dir).rglob("*.xml"):
+            xml_file = str(xml_file)
+            root = etree.parse(xml_file).getroot()
+            if (not (pmcid := check_text(root.xpath("//article-id[@pub-id-type='pmc']//text()")))
+                    or not (title := check_text(root.xpath("//title-group/article-title/text()")))
+                    or not (abstract := check_text(root.xpath("//abstract/p/text()")))
+                    or not (body_text := check_text(root.xpath("//body//text()")))):
+                continue
+            yield {
+                "pmcid": pmcid,
+                "title": title,
+                "abstract": abstract,
+                "body_text": body_text,
+            }
+    ds = Dataset.from_generator(iter_pmc_articles, cache_dir=r"C:\Users\School\Documents\cache", gen_kwargs = {"xml_dir": source})
+    ds.save_to_disk(target,max_shard_size = max_shard_size)
+    return ds
 
 def get_data_path(dataset_name: str, data_type: str) -> str | Path:
     if data_type != "dataset" and data_type != "embeddings":
@@ -24,8 +42,18 @@ def get_data_path(dataset_name: str, data_type: str) -> str | Path:
     project_root = Path(__file__).resolve().parent.parent
     with open(project_root / "config.json", "r", encoding="utf-8") as f:
         cfg = json.load(f)
-    return project_root / cfg["paths"]["data"].format(dataset_name=dataset_name, data_type=data_type)
+    return project_root / cfg["paths"]["data"].format(
+        dataset_name=dataset_name, data_type=data_type
+    )
+
 
 def load_local(dataset_name: str, data_type: str) -> Dataset | DatasetDict | Any:
     path = get_data_path(dataset_name, data_type)
-    return load_from_disk(path) if data_type == "dataset" else torch.load(path / "embeddings.pt")
+    return (
+        load_from_disk(path)
+        if data_type == "dataset"
+        else torch.load(path / "embeddings.pt")
+    )
+
+ds = xml_to_arrow(r"C:\Users\School\Documents\PMC\xml\PMC010xxxxxx", r"C:\Users\School\Documents\Processed\PMC010xxxxxx")
+print(ds[0])

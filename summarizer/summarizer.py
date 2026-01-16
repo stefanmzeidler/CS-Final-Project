c159084643a1bdff1de61745b06b867b596e1ae5
stefanmzeidler <stefanmzeidler@gmail.com>
2026-01-16 13:28:50 -0600

    Partial implementation of summarize function.

--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -1,14 +1,27 @@
 from docretriever import DocRetriever
+from llm import LLM
+import dataset_utils as du
 
 class Summarizer:
     def __init__(self, doc_retriever: DocRetriever):
         self.doc_retriever = doc_retriever
         self.doc_retriever.load_embeddings()
+        self.llm = LLM()
 
 
     def summarize(self, pmcid):
-        similar_docs = self.doc_retriever.retrieve_similar(pmcid)
-        print(similar_docs[0]['pmcid'])
+        paper = du.article_to_dict(pmcid)
+        supporting_docs = self.doc_retriever.supporting_docs(pmcid)
+        print(Summarizer._concat_docs(supporting_docs))
+        # self.llm.query_model(paper = paper, supporting_docs = supporting_docs, temperature=.8)
+    @staticmethod
+    def _concat_docs( supporting_docs):
+        docs = []
+        for index, doc in enumerate(supporting_docs):
+            docs.append(f"Document {index}: {du.clean_text(doc['title'])}")
+            docs.append(f"Abstract: {du.clean_text(doc['abstract'])}")
+            docs.append(f"Body text: {du.clean_text(doc['body_text'])}")
+        return "\n".join(docs)
 
 if __name__ == '__main__':
     from st_retriever import STRetriever

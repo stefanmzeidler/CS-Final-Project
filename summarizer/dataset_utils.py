8d5bd1b018ad213bf242a3d0e681b3d82e93f1cb
stefanmzeidler <stefanmzeidler@gmail.com>
2026-01-16 00:29:25 -0600

    Combined pubmed_utils and dataset_utils into one file. Implemented retrieval of article via Entrez. Improved xml parsing.
    The functions in both files shared several tasks.

--- summarizer/dataset_utils.py
+++ summarizer/dataset_utils.py
@@ -6,7 +6,19 @@ import json
 import torch
 import lxml
 from lxml import etree
+from Bio import Entrez, Medline
+import os
+from typing import List, Generator, Any
+import dataset_utils as du
+import lxml
+from lxml import etree
+from Bio.Entrez.Parser import DictionaryElement
+from dotenv import load_dotenv
+from http.client import HTTPResponse
+import re
 
+CORRECT_DOCTYPE = "<!DOCTYPE article PUBLIC \"-//NLM//DTD JATS (Z39.96) Journal Archiving and Interchange DTD with MathML3 v1.3 20210610//EN\" \"JATS-archivearticle1-3-mathml3.dtd\">"
+CORRECT_ARTICLE_HEADER = "<article xmlns:mml=\"http://www.w3.org/1998/Math/MathML\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" dtd-version=\"1.3\" xml:lang=\"en\" article-type=\"research-article\"><?properties open_access?>"
 
 def xml_to_arrow(source: str | Path, target: str | Path, max_shard_size = "100MB"):
     """
@@ -16,30 +28,40 @@ def xml_to_arrow(source: str | Path, target: str | Path, max_shard_size = "100MB
     :param max_shard_size: The maximum size of the shards to create. Must be in "100MB" format.
     """
     def iter_pmc_articles(xml_dir):
-        def check_text(text_list):
-            return None if len(text_list) == 0 else " ".join(text_list).strip()
-
         for xml_file in Path(xml_dir).rglob("*.xml"):
             try:
-                xml_file = str(xml_file)
-                root = etree.parse(xml_file).getroot()
-                pmcid = check_text(root.xpath("//article-id[@pub-id-type='pmc']//text()"))
-                title = check_text(root.xpath("//title-group/article-title/text()"))
-                abstract = check_text(root.xpath("//abstract/p/text()"))
-                body_text = check_text(root.xpath("//body//text()"))
+                yield parse_pmc_file(xml_file)
             except lxml.etree.XMLSyntaxError:
                 continue
-            if not pmcid or not title or not abstract or not body_text:
+            except RuntimeError:
                 continue
-            yield {
-                "pmcid": pmcid,
-                "title": title,
-                "abstract": abstract,
-                "body_text": body_text,
-            }
     ds = Dataset.from_generator(iter_pmc_articles, gen_kwargs = {"xml_dir": source})
     ds.save_to_disk(target,max_shard_size = max_shard_size)
     return ds
+
+def parse_pmc_file(xml_file, xml_string = False):
+
+    def check_text(text_list):
+        return None if len(text_list) == 0 else " ".join(text_list).strip()
+
+    if xml_string:
+        root = etree.fromstring(xml_file)
+    else:
+        xml_file = str(xml_file)
+        root = etree.parse(xml_file).getroot()
+    pmcid = check_text(root.xpath("//article-id[1]/text()[1]"))
+    title = check_text(root.xpath("//title-group[1]/article-title[1]/text()[1]"))
+    abstract = check_text(root.xpath("//abstract//text()"))
+    body_text = check_text(root.xpath("//body//text()"))
+    if not pmcid or not title or not abstract or not body_text:
+        raise RuntimeError("PMC XML parsing failed")
+    return {
+        "pmcid": pmcid,
+        "title": title,
+        "abstract": abstract,
+        "body_text": body_text,
+    }
+
 
 def get_data_path(dataset_name: str, data_type: str) -> str | Path:
     if data_type != "dataset" and data_type != "embeddings":
@@ -59,4 +81,21 @@ def load_local(dataset_name: str, data_type: str) -> Dataset | DatasetDict | Any
         if data_type == "dataset"
         else torch.load(path / "embeddings.pt")
     )
+
+def get_article(pmcid:str)->str:
+    load_dotenv()
+    Entrez.email = os.getenv("ENTREZ_EMAIL")
+    handle = Entrez.efetch(db="pmc",id=pmcid)
+    xml_string = HTTPResponse.read(handle).decode("utf-8")
+    handle.close()
+    return xml_string
+
+def article_to_string(pmcid:str):
+    xml_string = get_article(pmcid)
+    article_dict = parse_pmc_file(xml_string, xml_string = True)
+    return article_dict
+
+
+article = article_to_string("PMC12667371")
+print(article)
 

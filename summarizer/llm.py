0f0a643ccebc6054fa747cad8dc55a11f2987772
stefanmzeidler <stefanmzeidler@gmail.com>
2026-01-17 23:56:56 -0600

    Updated constructor allow specification of gemini model. Finished query_model implementation.
    Defaults to gemini-2.0-flash, but allows for changing models if desired.

--- summarizer/llm.py
+++ summarizer/llm.py
@@ -3,33 +3,55 @@ from google.genai import types
 import os
 from dotenv import load_dotenv
 import yaml
+from collections import defaultdict
+from typing import Dict
+
 
 class LLM:
-    def __init__(self, **kwargs):
+    def __init__(self, model_name="gemini-2.0-flash"):
+        print("Initializing LLM")
+        self.model_name = model_name
         load_dotenv()
         api_key = os.getenv("GEMINI_API_KEY")
         self.client = genai.Client(api_key=api_key)
+        print("Initialization done")
 
-    def query_model(self, paper, supporting_docs, temperature):
+    def query_model(
+        self,
+        query: str,
+        supporting_docs: str,
+        audiences: Dict[str, str],
+        max_words: int,
+        temperature: float,
+    ) -> Dict[str, str]:
+        print("Querying llm")
+        summaries = defaultdict(str)
         with open("prompts.yaml") as f:
             prompts = yaml.safe_load(f)
-        query = self.generate_query(prompts = prompts, name ="summarize_paper", audience ="lay person with an 8th grade reading level", max_words = 300, paper = paper, supporting_docs = supporting_docs)
-        return query
-        # response = self.client.models.generate_content(
-        #     model="gemini-2.0-flash",
-        #     contents=query["text"],
-        #     config= types.GenerateContentConfig(system_instruction=query["system"],temperature=temperature)
-        # )
-        # return response
+
+        for key, audience in audiences.items():
+            query = self._generate_query(
+                prompts=prompts,
+                name="summarize_paper",
+                audience=audience,
+                max_words=max_words,
+                paper=query,
+                supporting_docs=supporting_docs,
+            )
+            response = self.client.models.generate_content(
+                model=self.model_name,
+                contents=query["text"],
+                config=types.GenerateContentConfig(
+                    system_instruction=query["system"], temperature=temperature
+                ),
+            )
+            summaries[key] = response.text
+        print("Query done")
+        return summaries
 
     @staticmethod
-    def generate_query(prompts, name, **kwargs):
+    def _generate_query(prompts, name, **kwargs):
         p = prompts["prompts"][name]
         system = p["system"]
         template = p["template"]
         return {"system": system.format(**kwargs), "text": template.format(**kwargs)}
-
-
-if __name__ == "__main__":
-    my_llm = LLM()
-    my_llm.query_model("this is the doc", "this is supporting doc", temperature=0.8)

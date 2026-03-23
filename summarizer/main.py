b8a945235148139b6eafc6da331bc28bcc5820c6
stefanmzeidler <stefanmzeidler@gmail.com>
2026-03-22 23:17:49 -0500

    Removed extra print statement

--- summarizer/main.py
+++ summarizer/main.py
@@ -7,7 +7,6 @@ import sys
 if __name__ == "__main__":
     my_summarizer = Summarizer(STRetriever(),Reranker(),LLM())
     summaries = my_summarizer.summarize(sys.argv[1])
-    print(summaries)
     for audience,summary in summaries.items():
         print(f"\033[31mFor {audience} audience:\033[0m")
         print(summary+"\n")

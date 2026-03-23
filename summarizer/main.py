e46b671424212dc740cd1fe4b9d43bb92b3613e7
stefanmzeidler <stefanmzeidler@gmail.com>
2026-03-22 22:46:22 -0500

    Added main driver class.

---
+++ summarizer/main.py
@@ -0,0 +1,15 @@
+from summarizer import Summarizer
+from st_retriever import STRetriever
+from reranker import Reranker
+from llm import LLM
+import sys
+
+if __name__ == "__main__":
+    my_summarizer = Summarizer(STRetriever(),Reranker(),LLM())
+    summaries = my_summarizer.summarize(sys.argv[1])
+    print(summaries)
+    for audience,summary in summaries.items():
+        print(f"\033[31mFor {audience} audience:\033[0m")
+        print(summary+"\n")
+
+
